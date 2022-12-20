18d42cfce6e1885f00830174ce0dc29d2cd31a6f
Steve Khong <116337071+steve-bannershop@users.noreply.github.com>
2022-12-20 17:17:26 +0800

    Installed plugin
    - ACF
    - User Role Editor
    - Webp Converter For Media

---
+++ wp-content/plugins/webp-converter-for-media/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gbiorczyk/webp-converter-for-media',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f393f2fb4c9280fa3c5373c3c116570914add3ce',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'gbiorczyk/webp-converter-for-media' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f393f2fb4c9280fa3c5373c3c116570914add3ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'matt-plugins/deactivation-modal' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '142f0b6c62b561555ed53b2fc030869869646cff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../matt-plugins/deactivation-modal',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
